d5282e4fb86d700555745de671494eaa40467f8e
ledenis <ledenis.git@gmail.com>
2014-02-28 16:29:07 +0000

    new class State: constructor & cloneAndAdd()

---
+++ tile-puzzle/src/main/State.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class State {
+	List<Grid> route; // From start to current grid
+	int g; // Actual cost
+	int h; // From current to goal (heuristic)
+
+	private State() {
+	}
+
+	/**
+	 * New state with a singleton route
+	 * 
+	 * @param start
+	 * @param h
+	 *            heuristic
+	 */
+	public State(Grid start, int h) {
+		route = new LinkedList<>();
+		route.add(new Grid(start));
+		g = 0;
+		this.h = h;
+	}
+
+	public State cloneAndAdd(Grid next, int nextH) {
+		State clone = new State();
+
+		for (Grid gr : route) {
+			clone.route.add(gr);
+		}
+		clone.route.add(next);
+
+		clone.g = this.g + 1;
+		clone.h = nextH;
+
+		return clone;
+	}
+}
